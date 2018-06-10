e08655ebde570091dfb8eeb20f58da40cad9ad00
Itai Marziano <Itai.Marziano@emc.com>
2018-06-10 23:56:57 +0300

    Add results to Lambda Tests

--- src/main/java/academy/elqoo/java8/lambda/Lambda.java
+++ src/main/java/academy/elqoo/java8/lambda/Lambda.java
@@ -1,17 +1,36 @@
 package academy.elqoo.java8.lambda;
 
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 public class Lambda {
 
   public static List<String> filter(List<String> strings, Predicate<String> condition) {
-    return strings.stream().filter(condition).collect(Collectors.toList()); //return null;
+    return strings.stream().filter(condition).collect(Collectors.toList());
   }
 
-  public static List<String> sortStrings(List<String> strings, SecondLetterComparator<String> secondLetterComparator) { /*
+  public static String create(Supplier<String> supplier) {
+    return supplier.get();
+  }
+
+  public static Integer getStringLength(String s, Function<String, Integer> function) {
+    return function.apply(s);
+  }
+
+  public static int multiply(int a, int b, BinaryOperator<Integer> operation) {
+    return operation.apply(a, b);
+  }
+
+  public static List<String> sortStrings(List<String> strings, Comparator<String> comparator) {
+    strings.sort(comparator);
+    return strings;
+  }
+
+  public static List<String> sortStringsSecondLetterComprator(List<String> strings, SecondLetterComparator<String>
+      secondLetterComparator) { /*
   implement the method to sort using secondLetterComparator */
     return strings.stream()
         .sorted(secondLetterComparator::compare)
--- src/test/java/academy/elqoo/java8/labmda/LambdaTest.java
+++ src/test/java/academy/elqoo/java8/labmda/LambdaTest.java
@@ -1,14 +1,14 @@
 package academy.elqoo.java8.labmda;
 
 import static java.util.Arrays.asList;
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.junit.Assert.assertThat;
 import academy.elqoo.java8.lambda.Lambda;
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LambdaTest {
 
@@ -20,10 +20,39 @@ public class LambdaTest {
   }
 
   @Test
-  public void shouldSortStrings() {
+  public void sortStringsSecondLetterComprator() {
     List<String> input = Arrays.asList("CA", "FG", "AR", "DE", "BY", "EZ");
-    List<String> result = Lambda.sortStrings(input, (r, l) -> r.charAt(1) - l.charAt(1)); //insert lambda implement SecondLetterComparator
+    List<String> result = Lambda.sortStringsSecondLetterComprator(input, (r, l) -> r.charAt(1) - l.charAt(1)); //insert lambda
+    // implement SecondLetterComparator
     assertThat(result, is(equalTo(Arrays.asList("CA", "DE", "FG", "AR", "BY", "EZ"))));
+  }
+
+  @Test
+  public void shouldCreateAString() {
+    String bigString = Lambda.create(() -> "hello");
+    assertTrue(bigString.length() > 0);
+  }
+
+  @Test
+  public void extractStringSize() {
+    String myString = "This is great";
+    int length = Lambda.getStringLength(myString, String::length);
+    assertTrue(length == 13);
+  }
+
+  @Test
+  public void multiply() {
+    int a = 5;
+    int b = 6;
+    int result = Lambda.multiply(a, b, (integer, integer2) -> a * b);
+    assertTrue(result == 30);
+  }
+
+  @Test
+  public void shouldSortStrings() {
+    List<String> input = Arrays.asList("C", "F", "A", "D", "B", "E");
+    List<String> result = Lambda.sortStrings(input, Comparator.comparing(s -> s));
+    assertThat(result, is(equalTo(Arrays.asList("A", "B", "C", "D", "E", "F"))));
   }
 
 
